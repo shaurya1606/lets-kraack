32968a53fc4d50365389bb5ce3a763c5be1c8c13
shaurya1606 <shaurya0616@gmail.com>
2025-10-10 22:06:05 +0530

    configuration file setups/ init

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001',
+    },
+};
 
 export default nextConfig;
